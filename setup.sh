b8df00f75a6ded3d8986ae1fed3a93fce60e9a94
Sovannara Hak <hak.sovannara@gmail.com>
2015-02-26 10:17:23 +0100

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if ! `test x$(basename $(pwd)) = x.dotfiles`; then
+ echo "please run ./setup.sh from .dotfiles directory"
+ exit 1
+fi
+
+dotfiles=`find . -maxdepth 1 -type f -and -name '.*'`
+for i in $dotfiles; do
+    fname=`basename $i`
+    echo "Creating symbolic link: $HOME/$fname"
+    ln -sf $fname $HOME/$fname
+done
+
+echo "Creating symbolic link: $HOME/.vimrc"
+ln -sf vim-config/_vimrc $HOME/.vimrc
+echo "Creating symbolic link: $HOME/.gvimrc"
+ln -sf vim-config/_gvimrc $HOME/.gvimrc
